26a38b6bcc4b4277feab1fc0a375227079ec4052
Hariz Roslan <harizroslan507@gmail.com>
2022-08-10 11:15:00 +0800

    Import and Added Like function to CardMenu

--- src/components/CardMenu.js
+++ src/components/CardMenu.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import "../styles/cardmenu.scss"
-import {ReactComponent as Inbox} from "../images/inbox.svg";
-import {ReactComponent as Comments} from "../images/comments.svg";
-import {ReactComponent as Notifications} from "../images/notifications.svg";
-import {ReactComponent as Bookmark} from "../images/bookmark.svg";
+import React from "react";
+import "../styles/cardmenu.scss";
+import { ReactComponent as Inbox } from "../images/inbox.svg";
+import { ReactComponent as Comments } from "../images/comments.svg";
+import { ReactComponent as Notifications } from "../images/notifications.svg";
+import { ReactComponent as Bookmark } from "../images/bookmark.svg";
+import Like from "./Like";
 
 function CardMenu() {
   return (
     <div className="cardMenu">
       <div className="interactions">
-        <Notifications className="icon" />
+        {/* <Notifications className="icon" /> */}
+        <Like className="icon fa-heart" />
         <Comments className="icon" />
         <Inbox className="icon" />
       </div>
       <Bookmark className="icon" />
     </div>
-  )
+  );
 }
 
-export default CardMenu
+export default CardMenu;
