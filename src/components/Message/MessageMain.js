f1b2cc478b8e7b02610b0bbf4903ca68ec65dc0d
Hariz Roslan <harizroslan507@gmail.com>
2022-08-11 10:48:47 +0800

    remove ReactDOM and added MessageMain Function

--- src/components/Message/MessageMain.js
+++ src/components/Message/MessageMain.js
@@ -1,15 +1,8 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 import "./index.css";
 import MessageApp from "./MessageApp";
 import Navigation from "../Navigation";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <MessageApp />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
 function MessageMain() {
   return (
     <React.StrictMode>
