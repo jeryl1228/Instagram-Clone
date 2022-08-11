2a6869fb0a0b09591590276f8fe9f8505b5e4728
firdaus <firdausihsan97@gmail.com>
2022-08-11 15:55:49 +0800

    fixed profile page bugs

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import "../styles/app.scss";
 import { Route, Routes } from "react-router-dom";
-import Login from "./Login";
 import Home from "./Home";
 import Inbox from "./Inbox";
 import CreatePost from "./CreatePost";
@@ -20,7 +19,7 @@ function App() {
         <Route path="/inbox" element={<Inbox />} />
         <Route path="/createpost" element={<CreatePost />} />
         <Route path="/notifications" element={<Notifications />} />
-        <Route path="/profile" element={<ProfileMain />} />
+        <Route path="/profile" element={<ProfileMain setUser={setUser} />} />
       </Routes>
     </UserContext.Provider>
   );
