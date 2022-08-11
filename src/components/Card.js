1ef0653a87dcd352ee878576b7ba2deda2d94e7f
firdaus <firdausihsan97@gmail.com>
2022-08-11 16:27:55 +0800

    css fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,38 +1,37 @@
-import React, {useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import "../styles/card.scss";
 import Profile from "./Profile";
 import { ReactComponent as CardButton } from "../images/cardButton.svg";
 import CardMenu from "./CardMenu.js";
 import { db } from "../data/firebaseConfig";
-import firebase from "firebase/compat/app"
+import firebase from "firebase/compat/app";
 
 import { UserContext } from "./App";
 import ProfileIcon from "./ProfileIcon";
 
 function Card(props) {
-  let {
-    accountName,
-    image,
-    description,
-    cardId,
-  } = props;
-  const [ comments, setComments ] = useState([]);
-  const [ comment, setComment ] = useState('');
+  let { accountName, image, description, cardId } = props;
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
   const user = useContext(UserContext);
 
   useEffect(() => {
     let unsubscribe;
     if (cardId) {
-      unsubscribe = db.collection("card").doc(cardId).collection("comments").orderBy('timestamp', 'asc').onSnapshot((snapshot) => {
-         setComments(snapshot.docs.map((doc) => doc.data()));
+      unsubscribe = db
+        .collection("card")
+        .doc(cardId)
+        .collection("comments")
+        .orderBy("timestamp", "asc")
+        .onSnapshot((snapshot) => {
+          setComments(snapshot.docs.map((doc) => doc.data()));
         });
-      }
-
+    }
 
     return () => {
       unsubscribe();
     };
-  }, [cardId]); 
+  }, [cardId]);
 
   const postComment = (event) => {
     event.preventDefault();
@@ -40,11 +39,10 @@ function Card(props) {
     db.collection("card").doc(cardId).collection("comments").add({
       text: comment,
       username: user.displayName,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
-    setComment('');    
-  }
-
+    setComment("");
+  };
 
   return (
     <div className="card">
@@ -75,26 +73,27 @@ function Card(props) {
       <div className="addComment">
         {/* <div className="commentText">Add a comment...</div>
         <div className="postText">POST</div> */}
-        {comments.map((comment)=>(
-          <p>
-            <b className="commentTag">{comment.username}</b>
-          {comment.text}</p>
+        {comments.map((comment) => (
+          <p className="commentContainer">
+            <strong className="commentTag">{comment.username}</strong>
+            {comment.text}
+          </p>
         ))}
         <form className="card_commentBox">
-          <input 
-          className="comment_input" 
-          type="text" 
-          placeholder="Add your comment" 
-          value={comment}
-          onChange={(event) => setComment(event.target.value)}
+          <input
+            className="comment_input"
+            type="text"
+            placeholder="Add your comment"
+            value={comment}
+            onChange={(event) => setComment(event.target.value)}
           />
           <button
-          className="comment_button"
-          disabled={!comment}
-          type="submit"
-          onClick={postComment}
+            className="comment_button"
+            disabled={!comment}
+            type="submit"
+            onClick={postComment}
           >
-          Comment
+            Comment
           </button>
         </form>
       </div>
