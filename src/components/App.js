3d7ea4cb590dea4bc2c781b966705366ba2ab967
Hariz Roslan <harizroslan507@gmail.com>
2022-08-11 10:21:05 +0800

    Import and Added path to MessageMain

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,17 @@ import "../styles/app.scss";
 import { Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Home from "./Home";
-import Inbox from "./Inbox";
+// import Inbox from "./Inbox";
 import CreatePost from "./CreatePost";
 import Notifications from "./Notifications.js";
 import ProfileMain from "./ProfilePage/ProfileMain";
+import MessageMain from "./Message/MessageMain";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/inbox" element={<Inbox />} />
+      <Route path="/inbox" element={<MessageMain />} />
       <Route path="/createpost" element={<CreatePost />} />
       <Route path="/notifications" element={<Notifications />} />
       <Route path="/profile" element={<ProfileMain />} />
